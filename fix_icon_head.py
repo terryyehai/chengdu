23a8b0021d1df06e4aca378e9501cd935900b8cd
YILE.NET\terryyeh <terryyeh@yile.com.tw>
2026-02-26 12:40:57 +0800

    fix(html): permanently fix missing apple-touch-icon head tags on all pages

--- fix_icon_head.py
+++ fix_icon_head.py
@@ -1,11 +1,10 @@
 ﻿import os
 import glob
-import re
 
-new_head_tags = '''  <link rel="manifest" href="./manifest.json" />
+new_head_tags = '''<link rel="manifest" href="./manifest.json" />
   <link rel="icon" href="./panda-icon-192.png" type="image/png" />
   <link rel="apple-touch-icon" href="./panda-icon-192.png" />
-'''
+  '''
 
 files = glob.glob('*.html')
 for file in files:
@@ -14,10 +13,10 @@ for file in files:
 
     # ensure new tags don't exist yet
     if 'panda-icon-192.png' not in content:
-        # replace theme-color tag with itself + the new tags
-        content = re.sub(r'(<meta name="theme-color" content=".*?" />)', r'\1\n' + new_head_tags, content)
+        # Let's just replace '</head>' with the tags + '</head>'
+        content = content.replace('</head>', new_head_tags + '</head>')
         
         with open(file, 'w', encoding='utf-8') as f:
             f.write(content)
 
-print("Icon head tags successfully re-injected using regex sub")
+print("Icon head tags successfully re-injected using replace </head>")
