89f09faa8a7f11af1a4c09c9ab054119181468d7
YILE.NET\terryyeh <terryyeh@yile.com.tw>
2026-02-26 12:38:34 +0800

    fix(html): re-inject apple-touch-icon head tags again via raw regex

--- fix_icon_head.py
+++ fix_icon_head.py
@@ -1,5 +1,6 @@
 ﻿import os
 import glob
+import re
 
 new_head_tags = '''  <link rel="manifest" href="./manifest.json" />
   <link rel="icon" href="./panda-icon-192.png" type="image/png" />
@@ -13,10 +14,10 @@ for file in files:
 
     # ensure new tags don't exist yet
     if 'panda-icon-192.png' not in content:
-        # Easy insert right before <link rel="preconnect"
-        content = content.replace('  <link rel="preconnect"', new_head_tags + '  <link rel="preconnect"')
+        # replace theme-color tag with itself + the new tags
+        content = re.sub(r'(<meta name="theme-color" content=".*?" />)', r'\1\n' + new_head_tags, content)
         
         with open(file, 'w', encoding='utf-8') as f:
             f.write(content)
 
-print("Icon head tags successfully re-injected using replace")
+print("Icon head tags successfully re-injected using regex sub")
