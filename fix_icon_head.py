0b8441ce661b4f688da2f6b1e37992fdce05c5b4
YILE.NET\terryyeh <terryyeh@yile.com.tw>
2026-02-26 12:37:38 +0800

    fix(html): re-inject lost apple-touch-icon head tags to all pages

---
+++ fix_icon_head.py
@@ -0,0 +1,22 @@
+﻿import os
+import glob
+
+new_head_tags = '''  <link rel="manifest" href="./manifest.json" />
+  <link rel="icon" href="./panda-icon-192.png" type="image/png" />
+  <link rel="apple-touch-icon" href="./panda-icon-192.png" />
+'''
+
+files = glob.glob('*.html')
+for file in files:
+    with open(file, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    # ensure new tags don't exist yet
+    if 'panda-icon-192.png' not in content:
+        # Easy insert right before <link rel="preconnect"
+        content = content.replace('  <link rel="preconnect"', new_head_tags + '  <link rel="preconnect"')
+        
+        with open(file, 'w', encoding='utf-8') as f:
+            f.write(content)
+
+print("Icon head tags successfully re-injected using replace")
