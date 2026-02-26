fd963aef57c88ebcc278f7c21ef4934461e9f3c8
YILE.NET\terryyeh <terryyeh@yile.com.tw>
2026-02-26 13:00:28 +0800

    fix(html): clean up literal '\n' before </head> injected by previous script

---
+++ clean_newlines.py
@@ -0,0 +1,13 @@
+﻿import glob
+
+files = glob.glob('*.html')
+for file in files:
+    with open(file, 'r', encoding='utf-8') as f:
+        content = f.read()
+        
+    content = content.replace('\\n</head>', '\n</head>')
+    
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+print("Literal '\\n' artifacts cleaned from headers")
